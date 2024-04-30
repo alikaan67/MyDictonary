5e2b8f5ea63f83f6185de0d57b78fc96d63a40ca
alika <alika@ALI>
2024-04-30 10:38:55 +0300

    Add project files.

---
+++ MyDictonary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class MyDictionary<TKey, TValue>
+{
+    public TKeay[] keys;
+    public TValue[] values;
+    public int count = 0;
+
+    public MyDictionary()
+    {
+        keys = new TKey[0];
+        values = new TValue[0];
+        count = 0;
+    }
+    public void Add()
+    {
+        //Yeni bir geçici dizi oluşturuyoruz
+        TKey[] tempKeys = new TKey[count + 1];
+        TValue[] tempValues = new TValue[count + 1];
+
+        //mevcut anahtar ve değerleri geçici diziye kopyala
+
+        for(int i=0; i<count; i++)
+        {
+            tempKeys[i] = keys[i];
+            tempValues[i] = values[i];
+        }
+        //Yeni anahtar ve değeri ekle
+        tempKeys = keys;
+        tempValues = values;
+
+        //Anahtar ve değer dizilerini güncelle
+        keys = tempKeys;
+        values = tempValues;
+    }
+}
+
